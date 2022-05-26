2362ae5e82abcb83843960bcb610a85a41b1e070
William Harrison <william@williamdavidharrison.com.au>
2022-05-27 06:58:00 +0800

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import bodyParser from "body-parser";
+import express from "express";
+
+const app = express();
+const port = process.env.port || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
+app.use(bodyParser.text({ type: "text/html" }));
+
+app.get("/", async (req, res) => {
+  res.redirect("https://williamdavidharrison.com.au");
+})
+
+app.listen(port, () => {
+  console.log(`API Listening on Port: ${port}`);
+})

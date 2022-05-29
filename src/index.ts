9bcd9fe13ff6fb1ef411b14eac944767ca983588
William Harrison <william@williamdavidharrison.com.au>
2022-05-30 06:53:56 +0800

    Add some endpoints with redirects

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,14 @@ app.get("/", async (req, res) => {
   res.redirect("https://williamdavidharrison.com.au");
 })
 
+app.get("/cdn", async (req, res) => {
+  res.redirect("https://cdn.williamharrison.xyz");
+})
+
+app.get("/status", async (req, res) => {
+  res.redirect("https://status.williamharrison.xyz");
+})
+
 app.get("/test", async (req, res) => {
   res.send("API is Online!");
 })

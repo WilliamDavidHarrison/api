ace0b3f9713bcf20098d345f72d6b86d3d1d1aa4
William Harrison <william@williamdavidharrison.com.au>
2022-05-30 17:37:28 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 
@@ -6,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 
-const router = require("./util/router");
+const router = require("./util/router.js");
 const config = require("./config.json");
 
 app.use("/", router);
